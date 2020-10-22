aad0de3763535f8854ff6984ed427ae526890753
mathalaimuthucode <73230173+mathalaimuthucode@users.noreply.github.com>
2020-10-22 13:41:52 +0530

    Updated the Datatable source

--- XamariniOSDataTable/ViewController.cs
+++ XamariniOSDataTable/ViewController.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using UIKit;
-
 using System.Data;
 
 namespace XamariniOSDataTable
@@ -21,7 +20,6 @@ namespace XamariniOSDataTable
 		partial void UIButton16_TouchUpInside(UIButton sender)
 		{
 			DataTable table = GetValue(txtValue1.Text, txtValue2.Text);
-
 			foreach (DataRow row in table.Rows)
 			{
 				lblValue1.Text = row[0].ToString();
@@ -32,20 +30,17 @@ namespace XamariniOSDataTable
 		partial void UIButton11_TouchUpInside(UIButton sender)
 		{
 			DataTable table = GetValue(txtValue1.Text, txtValue2.Text);
-
 			UIAlertView alert = new UIAlertView("Success", "Data has been Stored Successfully.", null, "Close", null);
 			alert.Show();
 		}
 
+		/// Get the values form DataTable  
 		public DataTable GetValue(string v1,string v2)
 		{
 			DataTable table = new DataTable();
-
 			table.Columns.Add("Value1", typeof(string));
 			table.Columns.Add("Value2", typeof(string));
-
 			table.Rows.Add(v1, v2);
-
 			return table;
 		}
 
